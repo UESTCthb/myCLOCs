88cc767e04f716df153e41902991865937ad9271
UESTCthb <1465870296@qq.com>
2024-05-01 16:36:44 +0800

    CLOCS

---
+++ second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/indice.cu" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/indice.cu.o"
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/maxpool.cu" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/maxpool.cu.o"
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/reordering.cu" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/reordering.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "SPCONV_CUDA"
+  "spconv_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-10.2/targets/x86_64-linux/include"
+  "../../include"
+  "/home/uestc8/.conda/envs/THB/lib/python3.6/site-packages/torch/include"
+  "/home/uestc8/.conda/envs/THB/lib/python3.6/site-packages/torch/include/torch/csrc/api/include"
+  "/usr/local/cuda-10.2/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/all.cc" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/all.cc.o"
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/indice.cc" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/indice.cc.o"
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/maxpool.cc" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/maxpool.cc.o"
+  "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/src/spconv/reordering.cc" "/home/uestc8/THB/CLOCs/second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/spconv/CMakeFiles/spconv.dir/reordering.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SPCONV_CUDA"
+  "spconv_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-10.2/targets/x86_64-linux/include"
+  "../../include"
+  "/home/uestc8/.conda/envs/THB/lib/python3.6/site-packages/torch/include"
+  "/home/uestc8/.conda/envs/THB/lib/python3.6/site-packages/torch/include/torch/csrc/api/include"
+  "/usr/local/cuda-10.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ second/spconv_8da6f96/build/temp.linux-x86_64-3.6/src/utils/CMakeFiles/spconv_nms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../../lib.linux-x86_64-3.6/spconv/libspconv_nms.pdb"
+  "../../../lib.linux-x86_64-3.6/spconv/libspconv_nms.so"
+  "../../../lib.linux-x86_64-3.6/spconv/libspconv_nms.so.1"
+  "../../../lib.linux-x86_64-3.6/spconv/libspconv_nms.so.1.0"
+  "CMakeFiles/spconv_nms.dir/nms.cu.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/spconv_nms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
